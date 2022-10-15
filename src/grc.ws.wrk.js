099d5c878ddc70b44ebbd0e7361370b26f2df509
vigan-abd <vigan.abd@gmail.com>
2022-10-15 17:12:39 +0200

    added support for ws

---
+++ src/grc.ws.wrk.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const GrcWrkBase = require('./grc.wrk.base')
+const { PeerRPCServer } = require('grenache-nodejs-ws')
+
+class GrcWsWrk extends GrcWrkBase {
+  /**
+   * @param {Object} opts
+   * @param {string} opts.name - Grc service name
+   * @param {number} opts.port - Grc service port
+   * @param {string} opts.grape - Grape URL
+   * @param {number} [opts.timeout] - Grc call timeout
+   * @param {number} [opts.announce] - Grc announce interval
+   * @param {Object} [opts.conf] - Worker config
+   * @param {string} [opts.env] - Worker environment
+   */
+  constructor ({ name, port, grape, timeout = 30000, announce = 15000, conf = {}, env = 'development' }) {
+    super({ name, port, grape, announce, conf, env })
+
+    this._peerServer = new PeerRPCServer(this._link, { timeout })
+  }
+}
+
+module.exports = GrcWsWrk
