340df590b8a5f394680992c2ff6d5db1bf10533e
vigan-abd <vigan.abd@gmail.com>
2022-10-15 17:15:39 +0200

    fix

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const GrcWrkBase = require('./src/grc.wrk.base')
 const GrcHttpWrk = require('./src/grc.http.wrk')
+const GrcWsWrk = require('./src/grc.ws.wrk')
 
 module.exports = {
   GrcWrkBase,
-  GrcHttpWrk
+  GrcHttpWrk,
+  GrcWsWrk
 }
